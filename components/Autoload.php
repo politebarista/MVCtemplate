a4ff3f33222901bce1540aa2a1601a924d88ae20
Ilya <63463572+politebarista@users.noreply.github.com>
2020-08-07 16:29:29 +0700

    Add files via upload

--- components/Autoload.php
+++ components/Autoload.php
@@ -1,26 +1,11 @@
 <?php
 
-/**
- * Функция __autoload для автоматического подключения классов
- */
-function __autoload($class_name)
-{
-    // Массив папок, в которых могут находиться необходимые классы
-    $array_paths = array(
-        '/models/',
-        '/components/',
-        '/controllers/',
-    );
+define('CTRL', ROOT . '/controllers');
+define('CFG', ROOT . '/config');
+define('CMP', ROOT . '/components');
 
-    // Проходим по массиву папок
-    foreach ($array_paths as $path) {
-
-        // Формируем имя и путь к файлу с классом
-        $path = ROOT . $path . $class_name . '.php';
-
-        // Если такой файл существует, подключаем его
-        if (is_file($path)) {
-            include_once $path;
-        }
-    }
-}
+spl_autoload_register(function ($class_name) {
+    if (file_exists(CTRL . '/' . $class_name . '.php')) include CTRL . '/' . $class_name . '.php';
+    if (file_exists(CFG . '/' . $class_name . '.php')) include CFG . '/' . $class_name . '.php';
+    if (file_exists(CMP . '/' . $class_name . '.php')) include CMP . '/' . $class_name . '.php';
+});
